103b34e7f3e47722b17901af970478811c06b09a
egjallow10 <egjallow10@gmail.com>
2025-08-13 17:17:06 +0000

    calculate delta

--- CSCE/hack_1.c
+++ CSCE/hack_1.c
@@ -27,8 +27,14 @@ int main(void)
 	lat2 = lat2 * M_PI / 180.0;
 	lon2 = lon2 * M_PI / 180.0;
 
+	//Calculate delta longitude
+	double delta = lon2 - lon1;
+
+
+
 	printf("Origin: %f %f\n", lat1, lon1);
 	printf("Des: %f %f\n", lat2, lon2);
+	printf("Delta: %f ", delta);
 	return 0;
 
 }
